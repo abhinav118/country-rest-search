2588583b41b909ca03da73e49db46cbceed4fc94
Надежда Титова <titova.nadejda.job@gmail.com>
2018-08-01 21:26:54 +0300

    test heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const data = require('./data.json');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -64,5 +65,14 @@ app.get('/api/country', (req, res) => {
 	// 	});
 	// });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
